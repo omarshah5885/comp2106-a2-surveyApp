1c9ac2ac8f07df3cebe70c7dca80da0aec7995c6
omarshah5885 <200323449@student.georgianc.on.ca>
2019-03-23 00:41:15 -0400

    added db connection, navbar, user model, register page

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+// var flash = require('connect-flash');
+// var session = require('express-session');
+// var passport = require('passport');
 
+// connect to mongoose db
+mongoose.connect(
+  'mongodb+srv://user:Abc123!@cluster0-rltl0.mongodb.net/test?retryWrites=true ',
+  {
+    useNewUrlParser: true
+  }
+);
+
+// grab mongoose connection to listen to error/open events
+var db = mongoose.connection;
+
+// console log the error when db emits an error
+db.on('error', console.error.bind(console, 'Connection Error'));
+
+// console log connected once when db emits open
+db.once('open', callback => console.log('Connected to mongodb'));
+
+
+// route files
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +41,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+let User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resourcez');
 });
+
+// Register Form
+router.get('/register', function(req, res){
+  res.render('register');
+});
+// POST register process
+
+
 
 module.exports = router;
