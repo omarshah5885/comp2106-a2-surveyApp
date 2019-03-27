8c4d90645e511a62a9f018d5c278bd86cb3e58bf
omarshah5885 <200323449@student.georgianc.on.ca>
2019-03-27 18:48:29 -0400

    added dynamic functionality to form so that users can add questions; also processed options into db

---
+++ public/javascripts/script.js
@@ -0,0 +1,40 @@
+let addQuestion = document.querySelector('.btn-primary');
+let addOption = document.querySelector('.btn-danger');
+let questionCounter = 2;
+
+
+
+addQuestion.addEventListener('click', (e) => {
+  e.preventDefault();
+  const questionMarkup = `<div class="form-group col-md-4"><label for="questionType">Question Type</label><select class="form-control" id="questionType" name="questionType"><option selected="true" value="1">Multiple Choice</option><option value="2">Short Answer</option></select></div>
+  <div class="form-group"><label for="questionEntry">Question</label><input class="form-control" id="questionEntry" name="questionEntry" /></div>
+  <div class="multipleChoice${questionCounter}">
+  <div class="form-group"><input class="form-control" id="option${questionCounter}" name="option${questionCounter}" placeholder="enter option" /><input class="form-control" id="option${questionCounter}" name="option${questionCounter}" placeholder="enter option" /></div>
+</div>
+<div class="form-group"><button class="btn btn-danger" id="optionButton${questionCounter}" type="submit" data-index="${questionCounter}">Add Option</button></div>`;
+  const newQuestion = document.querySelector('.questionArea');
+  newQuestion.insertAdjacentHTML('beforeend', questionMarkup);
+
+  let addOption = document.querySelector(`#optionButton${questionCounter}`);
+
+  addOption.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(addOption);
+    const index = addOption.dataset.index;
+    const optionMarkup = `<input class="form-control" id="option${index}" name="option${index}" placeholder="enter option" />`;
+    const newOption = document.querySelector(`.multipleChoice${index} .form-group`);
+    newOption.insertAdjacentHTML('beforeend', optionMarkup);
+  });
+  questionCounter++
+  
+
+});
+
+addOption.addEventListener('click', (e) => {
+  e.preventDefault();
+  const optionMarkup = `<input class="form-control" id="option1" name="option1" placeholder="enter option" />`;
+  const newOption = document.querySelector('.multipleChoice .form-group');
+  newOption.insertAdjacentHTML('beforeend', optionMarkup);
+});
+
+
--- routes/surveys.js
+++ routes/surveys.js
@@ -5,11 +5,11 @@ const passport = require('passport');
 const {check} = require('express-validator/check');
 let Survey = require('../models/survey');
 let Question = require('../models/question');
-
+let MultipleChoice = require('../models/multiplechoice');
 
 // List of surveys 
 router.get('/', (req, res, next) => {
-  Survey.find({ endDate: { $lt: new Date() } }).then( surveys => {
+  Survey.find({ endDate: {$lt: new Date()} }).then( surveys => {
     res.render('surveys/surveylist', { surveys });
   });
 });
@@ -19,17 +19,36 @@ router.get('/create', (req, res, next) => {
   res.render('surveys/create', {});
 });
 
-// post survey 
+
+
+// process survey form
 router.post('/create', (req, res, next) => {
+  console.log(req.body);
   const newSurvey = new Survey();
   newSurvey.surveyTitle = req.body.surveyTitle;
   newSurvey.startDate = new Date();
   newSurvey.endDate = new Date();
   newSurvey.questions = [];
-  req.body.questionEntry.forEach( (questionEntry, index) => {
+
+  let questionEntry = req.body.questionEntry;
+  questionEntry.forEach( (questionEntry, index) => {
+    // create new question object per every question entry 
     const question = new Question();
+
+    // ensure questiontype indices matchup with each question entered into survey form
     question.questionType = req.body.questionType[index];
+
+    // ensuring each question entry from form is contained inside the question object 
     question.questionEntry = questionEntry;
+
+    if (question.questionType==1) {
+      question.multipleChoices = req.body['option' + ++index].map( item => {
+        const multiplechoice = new MultipleChoice();
+        multiplechoice.option = item;
+        return multiplechoice;
+      }); // end map      
+    } 
+    // pushing question object into the questions array that is contained in the survey object 
     newSurvey.questions.push(question);
   });
   newSurvey.save(err => {
